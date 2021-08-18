5756cdd503442da5f16c5e9ec392d9b2658e1b88
Adil <88982323+doritthy@users.noreply.github.com>
2021-08-18 21:56:53 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.CPUim = new System.Windows.Forms.PictureBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CPUim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LawnGreen;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(299, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(266, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Player: 0 - Comp: 0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(246, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 63);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "result";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.Location = new System.Drawing.Point(595, 445);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 43);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CPUim
+            // 
+            this.CPUim.Image = global::WindowsFormsApplication1.Properties.Resources.rock;
+            this.CPUim.Location = new System.Drawing.Point(564, 140);
+            this.CPUim.Name = "CPUim";
+            this.CPUim.Size = new System.Drawing.Size(129, 133);
+            this.CPUim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.CPUim.TabIndex = 7;
+            this.CPUim.TabStop = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Image = ((System.Drawing.Image)(resources.GetObject("checkBox3.Image")));
+            this.checkBox3.Location = new System.Drawing.Point(13, 292);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(146, 124);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Image = global::WindowsFormsApplication1.Properties.Resources.paper;
+            this.checkBox2.Location = new System.Drawing.Point(13, 164);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(145, 109);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Image = ((System.Drawing.Image)(resources.GetObject("checkBox1.Image")));
+            this.checkBox1.Location = new System.Drawing.Point(13, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(144, 133);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Orange;
+            this.button3.Location = new System.Drawing.Point(569, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 50);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Restart";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 524);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.CPUim);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CPUim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox CPUim;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = string.Empty;
+            
+            str[0] = "Rock";
+            str[1] = "Paper";
+            str[2] = "Scissors";
+        }
+        string[] str = new string[3];
+        Random r = new Random();
+
+        int playerScore;
+        int CompScore;
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) // отключаем остальные кнопки
+        {
+            if (checkBox1.Checked == true)  // игрок выбрал камень
+            {
+                checkBox2.Enabled = false;
+                checkBox3.Enabled = false; // другие варианты не выбираются
+            }
+            else
+            {
+                checkBox2.Enabled = true;
+                checkBox3.Enabled = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) //отключаем остальные кнопки
+        {
+            if (checkBox2.Checked == true)  // игрок выбрал бумагу
+            {
+                checkBox1.Enabled = false;
+                checkBox3.Enabled = false;
+            }
+            else
+            {
+                checkBox1.Enabled = true;
+                checkBox3.Enabled = true;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e) //отключаем остальные кнопки
+        {
+            if (checkBox3.Checked == true)  // игрок выбрал ножницы
+            {
+                checkBox2.Enabled = false;
+                checkBox1.Enabled = false;
+            }
+            else
+            {
+                checkBox2.Enabled = true;
+                checkBox1.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            if (checkBox1.Checked == true)
+            {
+                if (str[r.Next(0, 3)].Equals(str[0]))
+                {
+                    label2.Text = "Draw";
+                    CPUim.Image = Properties.Resources.rock;
+                }
+                if (str[r.Next(0, 3)].Equals(str[1]))
+                {
+                    
+                    label2.Text = "Computer Wins";
+                    CPUim.Image = Properties.Resources.paper;
+                    CompScore += 1;
+                }
+                if (str[r.Next(0, 3)].Equals(str[2]))
+                {
+                    label2.Text = "Player Wins";
+                    CPUim.Image = Properties.Resources.sciss;
+                    playerScore += 1;
+                }
+            }
+            if (checkBox2.Checked == true)
+            {
+                if (str[r.Next(0, 3)].Equals(str[0]))
+                {
+                    label2.Text = "Player Wins";
+                    CPUim.Image = Properties.Resources.rock;
+                    playerScore += 1;
+                }
+                if (str[r.Next(0, 3)].Equals(str[1]))
+                {
+                    label2.Text = "Draw";
+                    CPUim.Image = Properties.Resources.paper;
+                }
+                if (str[r.Next(0, 3)].Equals(str[2]))
+                {
+                    
+                    label2.Text = "Computer Wins";
+                    CPUim.Image = Properties.Resources.sciss;
+                    CompScore += 1;
+                }
+            }
+            if (checkBox3.Checked == true)
+            {
+                if (str[r.Next(0, 3)].Equals(str[0]))
+                {
+                    label2.Text = "Computer Wins";
+                    CPUim.Image = Properties.Resources.rock;
+                    CompScore += 1;
+                }
+                if (str[r.Next(0, 3)].Equals(str[1]))
+                {
+                    label2.Text = "Player Wins";
+                    CPUim.Image = Properties.Resources.paper;
+                    playerScore += 1;
+                }
+                if (str[r.Next(0, 3)].Equals(str[2]))
+                {
+                    label2.Text = "Draw";
+                    CPUim.Image = Properties.Resources.sciss;
+                }
+            }
+
+            else if(checkBox1.Checked == false && checkBox2.Checked == false && checkBox3.Checked == false)
+            {
+                MessageBox.Show("Please make a choice!");
+            }
+            label1.Text = "Player: " + playerScore + " - " + "Comp: " + CompScore;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            playerScore = 0;
+            CompScore = 0;
+            label1.Text = "Player: " + playerScore + " - " + "Comp: " + CompScore;
+        }
+    }
+}
